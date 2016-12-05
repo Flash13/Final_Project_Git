1f60b12905562a364a8497c3029e0801e4229d20
Abigail <aes6h2@mail.missouri.edu>
2016-12-05 17:12:35 -0600

    path.c is just code, not real functions

---
+++ path.c
@@ -0,0 +1,16 @@
+typedef struct node{
+	int x;
+	int y;
+}Step;
+
+//Put this in the loop that runs the robot
+int pathLen = 1;
+Step *Path = malloc(sizeof(Step));
+*(Path+0)->x = (starting x from file);
+*(Path+0)->y = (starting y from file);
+
+//Each time the robot moves
+pathLen++;
+Path = realloc(Path, pathLen);
+*(Path+(pathLen-1))->x = (x of new step);
+*(Path+(pathLen-1))->y = (y of new step);
