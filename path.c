fb866727cf5a0f6bc9e9fb14643e084d43217045
Abigail <aes6h2@mail.missouri.edu>
2016-12-06 17:54:28 -0600

    Fixed path.c...again

--- path.c
+++ path.c
@@ -1,16 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<ctype.h>
+#include<limits.h>
+#include<stdbool.h>
+
 typedef struct node{
 	int x;
 	int y;
 }Step;
 
-//Put this in the loop that runs the robot
-int pathLen = 1;
-Step *Path = malloc(sizeof(Step));
-*(Path+0)->x = (starting x from file);
-*(Path+0)->y = (starting y from file);
-
-//Each time the robot moves
-pathLen++;
-Path = realloc(Path, pathLen);
-*(Path+(pathLen-1))->x = (x of new step);
-*(Path+(pathLen-1))->y = (y of new step);
+int addToPath(Step *Path, int pathLen)
+{
+	//Each time the robot moves
+	pathLen++;
+	Path = realloc(Path, pathLen);
+	*(Path+(pathLen-1))->x = (x of new step);
+	*(Path+(pathLen-1))->y = (y of new step);
+	
+	return pathLen;
+}
