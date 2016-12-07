899cd0bd664f1341cdd0b129edfa8a688b2c9032
Abigail <aes6h2@mail.missouri.edu>
2016-12-06 18:29:08 -0600

    Updated path.c and changed types.h to match

--- path.c
+++ path.c
@@ -1,22 +1,34 @@
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
-#include<ctype.h>
-#include<limits.h>
-#include<stdbool.h>
+#include "types.h"
 
-typedef struct node{
-	int x;
-	int y;
-}Step;
 
-int addToPath(Step *Path, int pathLen)
+int main (void)
+{
+	int array[6] = {1,3,1,4,2,4};
+	
+	int length=1, i=0;
+	
+	Step *Path = malloc(sizeof(Step));
+	
+	length = addToPath(Path, length, array[0], array[1]);
+	length = addToPath(Path, length, array[2], array[3]);
+	length = addToPath(Path, length, array[4], array[5]);
+	
+	for(i=0; i<3; i++)
+		printf("Path[%d]: x=%d, y=%d\n", i, Path[i].x, Path[i].y);
+	
+	return 0;
+}
+//send the  Path pointer, the pathLen, bot->x, and bot->y 
+int addToPath(Step *Path, int pathLen, int newX, int newY)
 {
 	//Each time the robot moves
 	pathLen++;
-	Path = realloc(Path, pathLen);
-	*(Path+(pathLen-1))->x = (x of new step);
-	*(Path+(pathLen-1))->y = (y of new step);
+	Path = realloc(Path, pathLen); //add one to the number of structs in Path
+	(Path+(pathLen-2))->x = newX; //add bot->x to the new step's 'x'
+	(Path+(pathLen-2))->y = newY; //add bot->y to the new step's 'y'
 	
-	return pathLen;
+	return pathLen; //just to keep track, nothing needs to be done to pathLen outside of this function, just sent back and forth
 }
--- types.h
+++ types.h
@@ -3,26 +3,31 @@
 //dont forget to put other libraries if you need them
 
 typedef struct sq{
-int check;
+	int check;
 } Square;
 
 typedef struct obstacle{
-int speed;
-int x;
-int y;
-int ns;
-int ew;
+	int speed;
+	int x;
+	int y;
+	int ns;
+	int ew;
 } Obstacle;
 
 typedef struct robot{
-int x;
-int y;
+	int x;
+	int y;
 } Robot;
 
 typedef struct path{
-int x;
-int y;
+	int x;
+	int y;
 } Path;
+
+typedef struct node{
+	int x;
+	int y;
+}Step;
 
 int roomsize;
 
@@ -30,3 +35,4 @@ Square* createroom(FILE* filename);
 void loadroom(FILE* filename,Square *room, Robot*, Obstacle*, Obstacle*);
 Obstacle* loadob(FILE* fptr);
 int obmotion(Obstacle* object,Square *room);
+int addToPath(Step *Path, int pathLen, int newX, int newY); //in path.c
