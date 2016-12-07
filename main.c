1fca83f09f257cdb0291f42c1a45123dfddff6b5
Abigail <aes6h2@mail.missouri.edu>
2016-12-07 15:34:49 -0600

    The path taken by the robot is now saved to output.txt

--- main.c
+++ main.c
@@ -14,16 +14,23 @@ return 0;
 	Robot *bot = malloc(sizeof(Robot));
 	Obstacle *ob1 = malloc(sizeof(Obstacle));
 	Obstacle *ob2 = malloc(sizeof(Obstacle));
+	
+	FILE* ofp = fopen("output.txt", "w");
+	if(!ofp)
+		exit(FILE_FAILED_TO_OPEN);
+	
 	Path *exit = malloc(sizeof(Path));
 		loadroom(fp, bot, ob1, ob2, exit);
 	int stop = 0;
 		printf("\nRobot Starting Position: (%d,%d)",bot->x,bot->y);
+	fprintf(ofp, "Robot Starting Position: (%d,%d)\n", bot->x, bot->y);
 	while(stop != 1)
 		{
 	    obmotion(ob1);
 	    obmotion(ob2);
 		stop = robotmotion(bot,ob1,ob2,exit);
 		printf("\nRobot Position: (%d,%d)",bot->x,bot->y);
+		fprintf(ofp, "(%d,%d)\n", bot->x, bot->y);
 		}
 		printf("\nExit Position: (%d,%d)\n",exit->x,exit->y);
 return 0;
